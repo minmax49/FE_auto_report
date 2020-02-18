150d17530cf66a8697a6622af0ddab124a67f2ee
minmax49 <53674631+minmax49@users.noreply.github.com>
2020-02-18 11:54:38 +0700

    last_cimmit

--- refresh_excel.py
+++ refresh_excel.py
@@ -4,17 +4,6 @@ Created on Mon Jan 13 10:06:00 2020
 
 @author: nguyenquangminh3
 
-
-Sub aa()
-Sheets("EA-Provinces").Range("AR11").Value = 3
-        
-   Call MAP_DATA
-   Call Fill_color
-End Sub
-
-# --- 
-Còn thiếu refresh các sheet dashboard
-
 """
 
 import time 
@@ -25,9 +14,7 @@ import os
 import datetime
 import shutil
 import pickle
-import cx_Oracle
 import send_mail
-
 
 
 def refresh_pivot(wb , sheet='Overview', pivot_name='PivotTable2'):
@@ -51,12 +38,12 @@ def refresh_pivot(wb , sheet='Overview', pivot_name='PivotTable2'):
         print(e)
     
 
-def update_rundate(wb , sheet='Overview', pivot_name='PivotTable2',filter_name = "RUN_DAY"):
+def update_rundate(wb , sheet='Overview', pivot_name='PivotTable2'):
     today = datetime.datetime.today().strftime('%d') 
     #wb.sheets['Overview'].range('B2').value = '09'
     try:
         wb.sheets[sheet].select()
-        x = wb.api.ActiveSheet.PivotTables(pivot_name).PivotFields(filter_name)
+        x = wb.api.ActiveSheet.PivotTables(pivot_name).PivotFields("RUN_DATE")
         x.ClearAllFilters()
         x.EnableMultiplePageItems = False
         x.CurrentPage = today
@@ -75,11 +62,9 @@ def export_img_func(path, name_img,sheet_name,range_cell ):
 
     
 
-def main(main_path = 'C:/Users/nguyenquangminh3/projects/CRC_report_01',
-        template_path= 'template.xlsx', 
-        save_path = "T:/DeptData/Collection_Report/EARLY COLLX/CS&A",
-        save_name ='CRC_ENTRY_REPORT',
-        mail_name = 'minh.nguyen.50@fecredit.com.vn'):
+def main(main_path = 'C:/Users/nguyenquangminh3/projects/Card_report_center',
+        template_path= 'template.xlsm', 
+        save_name ='Field Operation Report'):
     
     """
     main process ,  whit 3 steps : 
@@ -99,7 +84,8 @@ def main(main_path = 'C:/Users/nguyenquangminh3/projects/CRC_report_01',
         DESCRIPTION. The default is 'minh.nguyen.50@fecredit.com.vn'.
 
     """
-
+    os.chdir(main_path) 
+    save_path = pd.read_excel('setup.xlsx',sheet_name='main_setup')['save_path'].iloc[0]
 # =============================================================================
 #   step 1 : define paths 
 # =============================================================================
@@ -109,7 +95,7 @@ def main(main_path = 'C:/Users/nguyenquangminh3/projects/CRC_report_01',
         main_path = main_path[:-1]
     
     #set root path
-    os.chdir(main_path) 
+    
     temp_excel_path = main_path+ '/' + template_path
     
     # copy template 
@@ -122,25 +108,16 @@ def main(main_path = 'C:/Users/nguyenquangminh3/projects/CRC_report_01',
         excel_save_path = save_path + '/{} {}.{}'.format(save_name,today,template_path[-4:])
     else:
         excel_save_path = None
-    if not os.path.exists(main_path+ '/excel'):
-        os.mkdir(main_path+ '/excel')
-        time.sleep(3)
         
     shutil.copy(temp_excel_path,excel_path)
     refresh_df = pd.read_excel('setup.xlsx',sheet_name='refresh') 
     refresh_df.dropna(inplace=True)
     
     excel_img_df = pd.read_excel('setup.xlsx',sheet_name='excel_img') 
-    excel_img_df.dropna(subset = ['sheet_name'],inplace=True)
+    excel_img_df.dropna(inplace=True)
     
     rundate_df = pd.read_excel('setup.xlsx',sheet_name='rundate') 
     rundate_df.dropna(inplace=True)
-    
-    cus_width_df = pd.read_excel('setup.xlsx',sheet_name='custom_width') 
-    cus_width_df.dropna(inplace=True)
-    
-    
-    #print('set_up done!')
 # =============================================================================
 #   step 2 : refresh and shoot 
 # =============================================================================
@@ -149,63 +126,69 @@ def main(main_path = 'C:/Users/nguyenquangminh3/projects/CRC_report_01',
     
     # open workbook.
     wb = xw.Book(excel_path)
+       
+    connects = wb.api.Connections
+    for i in range(1,connects.Count+1):
+        try:
+            connects.Item(i).OLEDBConnection.Connection = "OLEDB;Provider=OraOLEDB.Oracle.1;Password=Nwpass_Rkcol_0819;Persist Security Info=True;User ID=Common;Data Source=dwproddc;"
+            connects.Item(i).OLEDBConnection.BackgroundQuery = False 
+            #print(connects.Item(i).name)
+        except Exception as e:
+            print(e)
+            continue
     
-    # if have pivots 
+    wb.api.RefreshAll()
     # refresh pivots
-    if len(refresh_df) > 0:
-        for i in range(len(refresh_df)):
-            line = refresh_df.iloc[i]
-            refresh_pivot(wb = wb,sheet=line['sheet_name'], pivot_name=line['pivot_name'])
-    # else refresh pivots is null : refresh all 
-    else:
-        connects = wb.api.Connections
-        for i in range(1,connects.Count+1):
-            try:
-                connects.Item(i).OLEDBConnection.Connection = "OLEDB;Provider=OraOLEDB.Oracle.1;Password=Nwpass_Rkcol_0819;Persist Security Info=True;User ID=Common;Data Source=dwproddc;"
-                #print(connects.Item(i).name)
-            except Exception as e:
-                print(e)
-                continue
-        wb.api.RefreshAll()
-    
+    """
+    for i in range(len(refresh_df)):
+        line = refresh_df.iloc[i]
+        refresh_pivot(wb = wb,sheet=line['sheet_name'], pivot_name=line['pivot_name'])
+    """
     # set rundate to day 
-    if len(rundate_df) > 0:
-        for i in range(len(rundate_df)):
-            line = rundate_df.iloc[i]
-            update_rundate(wb = wb,sheet=line['sheet_name'], 
-                           pivot_name=line['pivot_name'],
-                           filter_name = line["filter_name"] )
+    for i in range(len(rundate_df)):
+        line = rundate_df.iloc[i]
+        update_rundate(wb = wb,sheet=line['sheet_name'], pivot_name=line['pivot_name'])
       
     # auto fit columns
     for sheet in excel_img_df[excel_img_df.autofit==1].sheet_name.unique():
         wb.sheets[sheet].autofit('c')
-    print('auto fit done')
-    #special case width column 
-    if len(cus_width_df) > 0:
-        for i in range(len(cus_width_df)):
-            line = cus_width_df.iloc[i]
-            wb.sheets[line['sheet_name']].range(line['cell']).column_width =float(line['width'])
-    print('custom fit done')
+    # special case width column for sheet Overview
+    wb.sheets['Overview'].range("C:C").column_width  = 12.14
+    
     #call macro
-    """
     map_data = wb.macro('Module2.MAP_DATA')
     map_data()
     fill_color = wb.macro('Module1.Fill_color')
     fill_color()
-    """
+    
     print('--'*20)
     #print('complete resource preparation')
-    
     # save comment
-    LR_b3 = {'excel_path' : excel_save_path}
+
+    LR_b3 = {'FC-CRC1': round(wb.sheets['Overview'].range("H1").value*100,2),
+         'FC-CRC2': round(wb.sheets['Overview'].range("I1").value*100,2),
+         'FCS-CRC1': round(wb.sheets['Overview'].range("H2").value*100,2),
+         'FCS-CRC2': round(wb.sheets['Overview'].range("I2").value*100,2),
+        }
+    
+    for k in LR_b3.keys():
+        if LR_b3[k] > 0 : 
+            LR_b3[k]  = """<b style="color:'green'"> BETTER  {}%</b>""".format(abs(LR_b3[k]))  
+        elif LR_b3[k] < 0:
+            LR_b3[k]  = """<b style="color:'red'"> GAP {}%</b>""".format(abs(LR_b3[k]))  
+        else:
+            LR_b3[k]  = """<b>EQUAL</b>"""  
+    
+    LR_b3['excel_path'] = excel_save_path
     with open('comment.pickle', 'wb') as handle:
         pickle.dump(LR_b3, handle, protocol=pickle.HIGHEST_PROTOCOL)
-     
+        
     
     # save file and quit
     
     wb.save()
-    wb.app.quit()    
+    wb.close()
+    #wb.app.quit()    
      
     if save_path != None:
         shutil.copy(excel_path,excel_save_path)
@@ -225,12 +208,16 @@ def main(main_path = 'C:/Users/nguyenquangminh3/projects/CRC_report_01',
     for i in range(len(excel_img_df)):
         temp = excel_img_df.iloc[i]
         export_img_func(excel_path, temp['name_img'],temp['sheet_name'],temp['range_cell'] )
+   
     print(time.time()/60 - start , 'completed shooting images')
     
    
 # =============================================================================
 #   step 3 : send_mail
 # =============================================================================
+    mail_name = pd.read_excel('setup.xlsx',sheet_name='main_setup')['mail_from'].iloc[0]
+
+
     mail_list = pd.read_excel('setup.xlsx',sheet_name='main_setup')
     
     mail_to = str(mail_list['mail_to'].dropna().tolist())
@@ -238,6 +225,7 @@ def main(main_path = 'C:/Users/nguyenquangminh3/projects/CRC_report_01',
     
     mail_to = mail_to.replace("'","").replace("[","").replace("]","").replace(",",";")
     mail_cc = mail_cc.replace("'","").replace("[","").replace("]","").replace(",",";")
+
 
     send_mail.send(mail_name=mail_name,
                    main_path= main_path,
